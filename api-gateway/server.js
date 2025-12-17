3f66359896b92e5b8f641385ffc210a0ba6857fb
Nithin <vooturinithinkumar@gmail.com>
2025-12-17 16:47:36 +0530

    update logger

--- api-gateway/server.js
+++ api-gateway/server.js
@@ -11,25 +11,25 @@ const { validateToken } = require('./src/middleware/authenticateUser');
 const PORT = process.env.PORT;
 
 app.use('/api/auth-ms', (req,res,next) => {
-    logger.http(`REQUEST sent to service - ${process.env.AS}🔑  running on port ${process.env.ASPORT} `);
+    logger.http(`REQUEST sent to service - ${process.env.AS} 🔑  running on port ${process.env.ASPORT} `);
     next()
 }, createProxyMiddleware({ target: url.authServiceUrl, changeOrigin: true }));
 app.use('/api/users-ms', (req,res,next) => {
-    logger.http(`REQUEST sent to service - ${process.env.US}👤  running on port ${process.env.USPORT} `);
+    logger.http(`REQUEST sent to service - ${process.env.US} 👤  running on port ${process.env.USPORT} `);
     next();
 }, createProxyMiddleware({ target: url.userServiceUrl, changeOrigin: true }));
 app.use('/api/vehicles-ms', validateToken, (req, res, next) => {
     if (req.role) {
         req.headers['x-user-role'] = req.role;
     }
-    logger.http(`REQUEST sent to service - ${process.env.VS}🚛  running on port ${process.env.VSPORT} `);
+    logger.http(`REQUEST sent to service - ${process.env.VS} 🚛  running on port ${process.env.VSPORT} `);
     next();
 }, createProxyMiddleware({ target: url.vehicleServiceUrl, changeOrigin: true }));
 app.use('/api/trips-ms', validateToken, (req, res, next) => {
     if (req.role) {
         req.headers['x-user-role'] = req.role;
     }
-    logger.http(`REQUEST sent to service - ${process.env.TS} running on port ${process.env.TSPORT}`);
+    logger.http(`REQUEST sent to service - ${process.env.TS} 🌎🔄🚚 running on port ${process.env.TSPORT}`);
     next();
 }, createProxyMiddleware({ target: url.tripServiceUrl, changeOrigin: true }));
 app.use('/api/realTimeTrack-ms', validateToken, (req, res, next) => {

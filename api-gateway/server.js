3cf0d89eff3aaa317ab61c5b09d9561a4e0b36d0
Nithin <vooturinithinkumar@gmail.com>
2025-12-28 16:27:53 +0530

    removed unused serviceURLs, variables

--- api-gateway/server.js
+++ api-gateway/server.js
@@ -14,10 +14,12 @@ app.use('/api/auth-ms', (req,res,next) => {
     logger.http(`REQUEST sent to service - ${process.env.AS} 🔑  running on port ${process.env.ASPORT} `);
     next()
 }, createProxyMiddleware({ target: url.authServiceUrl, changeOrigin: true }));
+
 app.use('/api/users-ms', (req,res,next) => {
     logger.http(`REQUEST sent to service - ${process.env.US} 👤  running on port ${process.env.USPORT} `);
     next();
 }, createProxyMiddleware({ target: url.userServiceUrl, changeOrigin: true }));
+
 app.use('/api/vehicles-ms', validateToken, (req, res, next) => {
     if (req.role) {
         req.headers['x-user-role'] = req.role;
@@ -25,6 +27,7 @@ app.use('/api/vehicles-ms', validateToken, (req, res, next) => {
     logger.http(`REQUEST sent to service - ${process.env.VS} 🚛  running on port ${process.env.VSPORT} `);
     next();
 }, createProxyMiddleware({ target: url.vehicleServiceUrl, changeOrigin: true }));
+
 app.use('/api/trips-ms', validateToken, (req, res, next) => {
     if (req.role) {
         req.headers['x-user-role'] = req.role;
@@ -32,25 +35,20 @@ app.use('/api/trips-ms', validateToken, (req, res, next) => {
     logger.http(`REQUEST sent to service - ${process.env.TS} 🌎🔄🚚 running on port ${process.env.TSPORT}`);
     next();
 }, createProxyMiddleware({ target: url.tripServiceUrl, changeOrigin: true }));
-app.use('/api/realTimeTrack-ms', validateToken, (req, res, next) => {
-    if (req.role) {
-        req.headers['x-user-role'] = req.role;
-    }
-    logger.http(`REQUEST sent to service - ${process.env.RTS} running on port ${process.env.RTSPORT}`);
-    next();
-}, createProxyMiddleware({ target: url.realtimeServiceUrl, changeOrigin: true }));
+
 app.use('/api/notifications-ms', validateToken, (req, res, next) => {
     if (req.role) {
         req.headers['x-user-role'] = req.role;
     }
-    logger.http(`REQUEST sent to service - ${process.env.NS} running on port ${process.env.NSPORT}`);
+    logger.http(`REQUEST sent to service - ${process.env.NS} 📧 running on port ${process.env.NSPORT}`);
     next();
 }, createProxyMiddleware({ target: url.notificationServiceUrl, changeOrigin: true }));
+
 app.use('/api/maintenance-ms', validateToken, (req, res, next) => {
     if (req.role) {
         req.headers['x-user-role'] = req.role;
     }
-    logger.http(`REQUEST sent to service - ${process.env.MS} running on port ${process.env.MSPORT}`);
+    logger.http(`REQUEST sent to service - ${process.env.MS} 👨🏻‍🔧🛻 running on port ${process.env.MSPORT}`);
     next();
 }, createProxyMiddleware({ target: url.maintenanceServiceUrl, changeOrigin: true }));
 
--- api-gateway/src/config/serviceURL.js
+++ api-gateway/src/config/serviceURL.js
@@ -7,7 +7,6 @@ module.exports = {
     userServiceUrl: process.env.USER_SERVICE_URL,
     vehicleServiceUrl: process.env.VEHICLE_SERVICE_URL,
     tripServiceUrl: process.env.TRIP_SERVICE_URL,
-    realtimeServiceUrl: process.env.REALTIMETRACKING_SERVICE_URL,
     maintenanceServiceUrl: process.env.MAINTENANCE_SERVICE_URL,
     notificationServiceUrl: process.env.NOTIFICATION_SERVICE_URL
 }
